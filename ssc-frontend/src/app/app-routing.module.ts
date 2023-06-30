6b7cc02b11b91dffccb57f1e5d77ab0c1d16ce87
Mohamad AlJasem <m-aljasem@outlook.com>
2023-06-29 23:00:42 -0400

    Adding Dashboard

--- ssc-frontend/src/app/app-routing.module.ts
+++ ssc-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { TestComponent } from './pages/test/test.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'test', component: TestComponent},
+  {path: '', component: AppComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ssc-frontend/src/app/app.module.ts
+++ ssc-frontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { WidgetComponent } from './components/widget/widget.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { TestComponent } from './pages/test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    WidgetComponent,
+    HeaderComponent,
+    HomeComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
